beb87d6802df3560593b036baffa61997be8ed2f
Enzo Inglese <enzochamu@gmail.com>
2023-07-29 15:37:56 -0300

    Segunda pre entrega

--- index.js
+++ index.js
@@ -1,87 +1,67 @@
-const CandyBarSimple = {
-    precio : 6250,
-    incluye : "Mobiliario"
-}
-const CandyBarconDecoracion = {
-    precio : 11900,
-    incluye: "Mobiliario + 50 globos + imagen de la tematica + nombre del agasajado"
-}
-const DecoracionBasica = {
-    precio : 10000,
-    incluye : "Fondo principal + 4 telas en techo + Banderines de nombre y FC + Globo numero + 40 globos"
-}
-const DecoracionMedia = {
-    precio : 15500,
-    incluye : "Fondo principal con led + 6 telas en techo + Banderines de nombre y FC + Globo numero + 70 globos"
-}
-const DecoracionCompleta = {
-    precio : 20500,
-    incluye : "Fondo principal con led + 8 telas en techo + Banderines de nombre y FC + Globo numero + 100 globos"
-}
+const decoraciones=[
+    {Id: "A", Nombre: "Candy Bar Simple", 
+    Precio: 6250, 
+    Incluye: "Mobiliario"},
+    {Id: "B", 
+    Nombre: "Candy Bar Con Decoracion", 
+    Precio: 11900, 
+    Incluye: "Mobiliario + 50 globos + imagen de la tematica + nombre del agasajado"},
+    {Id: "C", 
+    Nombre: "Decoracion Basica", 
+    Precio: 10000, 
+    Incluye: "Fondo principal + 4 telas en techo + Banderines de nombre y FC + Globo numero + 40 globos"},
+    {Id: "D", 
+    Nombre: "DecoracionMedia", 
+    Precio: 15500, 
+    Incluye: "Fondo principal con led + 6 telas en techo + Banderines de nombre y FC + Globo numero + 70 globos"},
+    {Id: "E", 
+    Nombre: "Decoracion Completa" , 
+    Precio: 20500, 
+    Incluye: "Fondo principal con led + 8 telas en techo + Banderines de nombre y FC + Globo numero + 100 globos"}
+]
 
-function presupuestar(){
-    let presupuesto = prompt("elija opcion segun el presupuesto que quiera: A - B - C - D - E - X (Para Terminar)")
-        while(presupuesto != "X" ){
-            switch (presupuesto) {
-                case "A":
-                    alert("el precio requerido es $" + CandyBarSimple["precio"])
+function elejirOpcion(){
+    let elejir = prompt("Elija la opcion que desee: Presupuesto - Incluye - Descuentos - X(para cerrar)")
+        while(elejir != "X"){
+            switch (elejir) {
+                case "Presupuesto":
+                    presupuesto()
                     break;
-                case "B":
-                    alert("el precio requerido es $" + CandyBarconDecoracion["precio"])
+                case "Incluye":
+                    incluir()
                     break;
-                case "C":
-                    alert("el precio requerido es $" + DecoracionBasica["precio"])
-                    break;
-                case "D":
-                    alert("el precio requerido es $" + DecoracionMedia["precio"])
-                    break;
-                case "E":
-                    alert("el precio requerido es $" + DecoracionCompleta["precio"])
+                case "Descuentos":
+                descuento()
                     break;
                 default:
-                    alert("opcion incorrecta")
-            }
-        presupuesto = prompt("elija opcion segun el presupuesto que quiera: A - B - C - D - E - X (Para Terminar)")
+                    alert("Ingreso datos incorrectos")
         }
-}
-
-function incluye(){
-    let elementos = prompt("elija la opcion para conocer lo que incluya cada una: A - B - C - D - E - X(para terminar)")
-        while(elementos != "X"){
-            switch(elementos){
-                case "A":
-                    alert("Incluye: " + CandyBarSimple["incluye"])
-                    break;
-                case "B":
-                    alert("Incluye: " + CandyBarconDecoracion["incluye"])
-                    break;
-                case "C":
-                    alert("Incluye: " + DecoracionBasica["incluye"])
-                    break;
-                    case "D":
-                        alert("Incluye: " + DecoracionMedia["incluye"])
-                        break;
-                    case "E":
-                    alert("Incluye: " + DecoracionCompleta["incluye"])
-                    break;
-                default:
-                    alert("opcion incorrecta")
-            }
-        elementos = prompt("Esciba la opcion para conocer lo que incluya cada una: A - B - C - D - E - X(para terminar)")
+        elejir=prompt("Elija la opcion que desee: Presupuesto - Incluye - Descuentos - X(para cerrar)")
     }
 }
+
+function presupuesto(){
+    let precio = prompt("elija opcion segun el presupuesto que quiera: A - B - C - D - E")
+        const buscar = decoraciones.find(elemento => elemento.Id === precio)
+        alert("el precio de " + buscar.Nombre + " es $" + buscar.Precio)
+    } 
+function incluir(){
+    let incluye = prompt("elija opcion segun el presupuesto que quiera: A - B - C - D - E")
+        const buscar = decoraciones.find(elemento => elemento.Id === incluye)
+        alert(buscar. Nombre + " Incluye: " + buscar.Incluye)
+    } 
 
 function descuento(precio = prompt("Escriba la opcion correspondiente para seleccionar el precio: A - B - C - D - E"),formaDePago = prompt("escriba la forma de pago: Efectivo - Tarjeta de credito - Debito")){
         switch (precio) {
             case "A":
                 if (formaDePago == "Efectivo") {
-                    precioFinal = CandyBarSimple["precio"] * 0.95
+                    precioFinal = decoraciones[0].Precio * 0.95
                     alert("En efectivo se realiza un 5% de descuento y abonará: $" + Math.round(precioFinal))
                 } else if(formaDePago == "Tarjeta de credito"){
-                    precioFinal = CandyBarSimple["precio"] * 1.1
+                    precioFinal = decoraciones[0].Precio * 1.1
                     alert("Con tarjeta de credito tiene un 10% de recargo y se abonará: " + Math.round(precioFinal) + " o 3 cuotas de: $" + Math.round(precioFinal/3))
                 } else if(formaDePago=="Debito"){
-                    precioFinal = CandyBarSimple["precio"]
+                    precioFinal = decoraciones[0].Precio
                     alert("No tiene descuento ni recargo y abonará: $" + precioFinal)
                 } else{
                     alert("Forma de pago incorrecta, vuelva a intentar")
@@ -89,13 +69,13 @@ function descuento(precio = prompt("Escriba la opcion correspondiente para selec
                 break;
             case "B":
                 if (formaDePago == "Efectivo") {
-                    precioFinal = CandyBarconDecoracion["precio"] * 0.95
+                    precioFinal = decoraciones[1].Precio * 0.95
                     alert("En efectivo se realiza un 5% de descuento y abonará: $" + Math.round(precioFinal))
                 } else if(formaDePago == "Tarjeta de credito"){
-                    precioFinal = CandyBarconDecoracion["precio"] * 1.1
+                    precioFinal = decoraciones[1].Precio * 1.1
                     alert("Con tarjeta de credito tiene un 10% de recargo y se abonará: " + Math.round(precioFinal) + " o 3 cuotas de: $" + Math.round(precioFinal/3))
                 } else if(formaDePago=="Debito"){
-                    precioFinal = CandyBarconDecoracion["precio"]
+                    precioFinal = decoraciones[1].Precio
                     alert("No tiene descuento ni recargo y abonará: $" + precioFinal)
                 } else{
                     alert("Forma de pago incorrecta, vuelva a intentar")
@@ -103,13 +83,13 @@ function descuento(precio = prompt("Escriba la opcion correspondiente para selec
                 break;
             case "C":
                 if (formaDePago == "Efectivo") {
-                    precioFinal = DecoracionBasica["precio"] * 0.95
+                    precioFinal = decoraciones[2].Precio * 0.95
                     alert("En efectivo se realiza un 5% de descuento y abonará: $" + Math.round(precioFinal))
                 } else if(formaDePago == "Tarjeta de credito"){
-                    precioFinal = DecoracionBasica["precio"] * 1.1
+                    precioFinal = decoraciones[2].Precio * 1.1
                     alert("Con tarjeta de credito tiene un 10% de recargo y se abonará: " + Math.round(precioFinal) + " o 3 cuotas de: $" + Math.round(precioFinal/3))
                 } else if(formaDePago=="Debito"){
-                    precioFinal = DecoracionBasica["precio"]
+                    precioFinal = decoraciones[2].Precio
                     alert("No tiene descuento ni recargo y abonará: $" + precioFinal)
                 } else{
                     alert("Forma de pago incorrecta, vuelva a intentar")
@@ -117,13 +97,13 @@ function descuento(precio = prompt("Escriba la opcion correspondiente para selec
                 break;
             case "D":
                 if (formaDePago == "Efectivo") {
-                    precioFinal = DecoracionMedia["precio"] * 0.95
+                    precioFinal = decoraciones[3].Precio * 0.95
                     alert("En efectivo se realiza un 5% de descuento y abonará: $" + Math.round(precioFinal))
                 } else if(formaDePago == "Tarjeta de credito"){
-                    precioFinal = DecoracionMedia["precio"] * 1.1
+                    precioFinal = decoraciones[3].Precio * 1.1
                     alert("Con tarjeta de credito tiene un 10% de recargo y se abonará: " + Math.round(precioFinal) + " o 3 cuotas de: $" + Math.round(precioFinal/3))
                 } else if(formaDePago=="Debito"){
-                    precioFinal = DecoracionMedia["precio"]
+                    precioFinal = decoraciones[3].Precio
                     alert("No tiene descuento ni recargo y abonará: $" + precioFinal)
                 } else{
                     alert("Forma de pago incorrecta, vuelva a intentar")
@@ -131,13 +111,13 @@ function descuento(precio = prompt("Escriba la opcion correspondiente para selec
                 break;
             case "E":
                 if (formaDePago == "Efectivo") {
-                    precioFinal = DecoracionCompleta["precio"] * 0.95
+                    precioFinal = decoraciones[4].Precio * 0.95
                     alert("En efectivo se realiza un 5% de descuento y abonará: $" + Math.round(precioFinal))
                 } else if(formaDePago == "Tarjeta de credito"){
-                    precioFinal = DecoracionCompleta["precio"] * 1.1
+                    precioFinal = decoraciones[4].Precio * 1.1
                     alert("Con tarjeta de credito tiene un 10% de recargo y se abonará: " + Math.round(precioFinal) + " o 3 cuotas de: $" + Math.round(precioFinal/3))
                 } else if(formaDePago=="Debito"){
-                    precioFinal = DecoracionCompleta["precio"]
+                    precioFinal = decoraciones[4].Precio
                     alert("No tiene descuento ni recargo y abonará: $" + precioFinal)
                 } else{
                     alert("Forma de pago incorrecta, vuelva a intentar")
@@ -148,3 +128,5 @@ function descuento(precio = prompt("Escriba la opcion correspondiente para selec
                 break;
         } 
 }
+
+elejirOpcion()
